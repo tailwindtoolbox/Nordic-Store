515ad18a073d0806ea05c7c5f7c1785599e154dd
Praise Freedom Dike <dikepraise119@gmail.com>
2023-07-27 21:45:03 +0100

    create script.js file

---
+++ script.js
@@ -0,0 +1,29 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const orderButtons = document.querySelectorAll(".order-btn");
+    const orderForm = document.getElementById("orderForm");
+    const productSelect = document.getElementById("productSelect");
+  
+    // Function to toggle the order form visibility
+    function toggleOrderForm(productName) {
+      // Show the order form
+      orderForm.classList.remove("hidden");
+  
+      // Set the selected product in the dropdown
+      const option = document.createElement("option");
+      option.text = productName;
+      productSelect.innerHTML = "";
+      productSelect.add(option);
+  
+      // You can add more logic here to pre-fill other fields if needed
+    }
+  
+    // Add click event listeners to all order buttons
+    orderButtons.forEach((btn) => {
+      btn.addEventListener("click", () => {
+        const productName = btn.dataset.product;
+        toggleOrderForm(productName);
+      });
+    });
+  });
+  
