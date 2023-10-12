fc4baed089f77a4de1ef13ac9bdd424c6cbf90e2
Praise Freedom Dike <dikepraise119@gmail.com>
2023-10-12 05:20:13 +0100

    fix scroll into investmentPlanSection

--- script.js
+++ script.js
@@ -1,66 +1,80 @@
 // Function to open the order form with the selected product name
 function openOrderForm(productName, productImageSrc) {
-  document.getElementById('orderFormSection').style.display = 'block';
-  document.getElementById('selectedProduct').textContent = productName;
-  document.getElementById('selectedProductImage').src = productImageSrc;
+  document.getElementById("orderFormSection").style.display = "block";
+  document.getElementById("selectedProduct").textContent = productName;
+  document.getElementById("selectedProductImage").src = productImageSrc;
   // scroll to the order from section
-  const orderFormSection = document.getElementById('orderFormSection');
-  orderFormSection.scrollIntoView({ behavior: 'smooth' });
+  const orderFormSection = document.getElementById("orderFormSection");
+  orderFormSection.scrollIntoView({ behavior: "smooth" });
+}
+
+// Function to open the installmental plan form
+function scrollToInstallmentalPlan() {
+  const installmentalPaymentSection = document.getElementById(
+    "installmentalPaymentSection"
+  );
+  document.getElementById("confirmOrderModal").style.display = "none";
+  console.log("scroll down");
+  installmentalPaymentSection.scrollIntoView({ behavior: "smooth" });
 }
 
 // Function to open the product image popup
 function openProductImagePopup(imageSrc) {
-  const popup = document.getElementById('productImagePopup');
-  const image = document.getElementById('popupImage');
+  const popup = document.getElementById("productImagePopup");
+  const image = document.getElementById("popupImage");
   image.src = imageSrc;
-  popup.style.display = 'block';
+  popup.style.display = "block";
 }
 
 function closeProductImagePopup() {
-  document.getElementById('productImagePopup').style.display = 'none';
+  document.getElementById("productImagePopup").style.display = "none";
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-  const orderButtons = document.querySelectorAll('.order-button');
-  const orderCovers = document.querySelectorAll('.object-cover');
+document.addEventListener("DOMContentLoaded", function () {
+  const orderButtons = document.querySelectorAll(".order-button");
+  const orderCovers = document.querySelectorAll(".object-cover");
 
   // to open the order form when the order button is clicked
   orderButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-      const productName = button.getAttribute('data-product');
-      const productPrice = button.getAttribute('data-price');
-      const productImageSrc = button.getAttribute('data-image');
+    button.addEventListener("click", function () {
+      const productName = button.getAttribute("data-product");
+      const productPrice = button.getAttribute("data-price");
+      const productImageSrc = button.getAttribute("data-image");
 
       // Populate the confirmation section with product details
-      document.getElementById('confirmationProductName').textContent = productName;
-      document.getElementById('confirmationProductPrice').textContent = 'Price: ' + productPrice;
+      document.getElementById("confirmationProductName").textContent =
+        productName;
+      document.getElementById("confirmationProductPrice").textContent =
+        "Price: " + productPrice;
 
       // Show the confirmation modal (popup)
-      document.getElementById('confirmOrderModal').style.display = 'flex';
+      document.getElementById("confirmOrderModal").style.display = "flex";
       // Hide the order form
-      document.getElementById('orderFormSection').style.display = 'none';
+      document.getElementById("orderFormSection").style.display = "none";
 
       // Add event listener for the "Confirm Order" button in the confirmation section
-      document.getElementById('confirmOrderBtn').addEventListener('click', function () {
-        // Hide the confirmation modal (popup)
-        document.getElementById('confirmOrderModal').style.display = 'none';
-        // Show the order form
-        document.getElementById('orderFormSection').style.display = 'block';
-        openOrderForm(productName, productImageSrc);
-      });
+      document
+        .getElementById("confirmOrderBtn")
+        .addEventListener("click", function () {
+          // Hide the confirmation modal (popup)
+          document.getElementById("confirmOrderModal").style.display = "none";
+          // Show the order form
+          document.getElementById("orderFormSection").style.display = "block";
+          openOrderForm(productName, productImageSrc);
+        });
     });
   });
 
-  // to open the product image popup when the order cover is clicked  
+  // to open the product image popup when the order cover is clicked
   orderCovers.forEach(function (cover) {
-    cover.addEventListener('click', function () {
-      const imageSrc = cover.getAttribute('src');
+    cover.addEventListener("click", function () {
+      const imageSrc = cover.getAttribute("src");
       openProductImagePopup(imageSrc);
     });
   });
 
-  const orderForm = document.getElementById('orderForm');
-  orderForm.addEventListener('submit', function (event) {
+  const orderForm = document.getElementById("orderForm");
+  orderForm.addEventListener("submit", function (event) {
     event.preventDefault();
     // Handle the form submission here
     // You can use the form elements like:
